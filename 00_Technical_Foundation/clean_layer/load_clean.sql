6303e8142b18cad4a8b2d2ae3d7c373e98761815
ammflo-ops <ammany.florence@gmail.com>
2026-07-01 16:39:53 +0100

    Refactor SQL for data loading and normalization

--- 00_Technical_Foundation/clean_layer/load_clean.sql
+++ 00_Technical_Foundation/clean_layer/load_clean.sql
@@ -75,49 +75,48 @@ WHERE Order_Line_ID IS NOT NULL; -- Remove NULL Order_Line_ID values
 TRUNCATE TABLE NAVA_clean.fact_returns;
 
 INSERT INTO NAVA_clean.fact_returns (
-return_id,
-order_id,
-order_line_id,
-return_date,
-return_reason,
-return_amount,
-return_quantity
+	return_id,
+	order_id,
+	order_line_id,
+	return_date,
+	return_reason,
+	return_amount,
+	return_quantity
 )
 SELECT
-TRIM(Return_ID),
-TRIM(Order_ID),
-TRIM(Order_Line_ID),
-Return_Date,
-LOWER(TRIM(Return_Reason)), -- Normalize Return_Reason values
-Return_Amount,
-Return_Quantity
+	TRIM(Return_ID),
+	TRIM(Order_ID),
+	TRIM(Order_Line_ID),
+	Return_Date,
+	LOWER(TRIM(Return_Reason)), -- Normalize Return_Reason values
+	Return_Amount,
+	Return_Quantity
 FROM NAVA_raw.fact_returns
 WHERE Return_ID IS NOT NULL; -- Remove NULL Return_ID values 
-
 
 -- Truncate and Reload NAVA_clean.dim_customers table
 TRUNCATE TABLE NAVA_clean.dim_customers;
 
 INSERT INTO NAVA_clean.dim_customers (
-customer_id,
-customer_name,
-customer_segment,
-customer_first_order_date,
-postal_code
+	customer_id,
+	customer_name,
+	customer_segment,
+	customer_first_order_date,
+	postal_code
 )
 SELECT
-TRIM(Customer_ID),
-TRIM(Customer_Name),
-UPPER(TRIM(Customer_Segment)), -- Normalize Customer_Segment values
-Customer_First_Order_Date,
-TRIM(REPLACE(REPLACE(Postal_Code, CHAR(13), ''), CHAR(10), '')) -- Remove carriage returns from Postal_Code to ensure reliable joins
+	TRIM(Customer_ID),
+	TRIM(Customer_Name),
+	UPPER(TRIM(Customer_Segment)), -- Normalize Customer_Segment values
+	Customer_First_Order_Date,
+	TRIM(REPLACE(REPLACE(Postal_Code, CHAR(13), ''), CHAR(10), '')) -- Remove carriage returns from Postal_Code to ensure reliable joins
 FROM (
-SELECT
-*,
-ROW_NUMBER() OVER (
-PARTITION BY TRIM(Customer_ID)
-ORDER BY Customer_First_Order_Date ASC) AS flag_cstID
-FROM NAVA_raw.dim_customers
+	SELECT
+		*,
+		ROW_NUMBER() OVER (
+		PARTITION BY TRIM(Customer_ID)
+	ORDER BY Customer_First_Order_Date ASC) AS flag_cstID
+	FROM NAVA_raw.dim_customers
 WHERE Customer_ID IS NOT NULL) t -- Remove NULL Customer_ID values
 WHERE flag_cstID = 1; -- Select the earliest record per customer
 
@@ -125,39 +124,40 @@ WHERE flag_cstID = 1; -- Select the earliest record per customer
 TRUNCATE TABLE NAVA_clean.dim_location;
 
 INSERT INTO NAVA_clean.dim_location (
-postal_code,
-city,
-region,
-country
+	postal_code,
+	city,
+	region,
+	country
 )
 SELECT
-TRIM(REPLACE(REPLACE(Postal_Code, CHAR(13), ''), CHAR(10), '')), -- Remove carriage returns from Postal_Code to ensure reliable joins
-TRIM(City),
-TRIM(Region), 
-UPPER(TRIM(REPLACE(REPLACE(Country, CHAR(13), ''), CHAR(10), ''))) -- Clean and Normalize Country values
+	TRIM(REPLACE(REPLACE(Postal_Code, CHAR(13), ''), CHAR(10), '')), -- Remove carriage returns from Postal_Code to ensure reliable joins
+	TRIM(City),
+	TRIM(Region), 
+	UPPER(TRIM(REPLACE(REPLACE(Country, CHAR(13), ''), CHAR(10), ''))) -- Clean and Normalize Country values
 FROM NAVA_raw.dim_location
 WHERE Postal_Code IS NOT NULL; -- Remove NULL Postal_Code values
 
 
 -- Truncate and Reload NAVA_clean.dim_products table
 TRUNCATE TABLE NAVA_clean.dim_products;
+
 INSERT INTO NAVA_clean.dim_products (
-product_id,
-product_name,
-category,
-sub_category,
-product_tier,
-is_top_seller,
-standard_cost
+	product_id,
+	product_name,
+	category,
+	sub_category,
+	product_tier,
+	is_top_seller,
+	standard_cost
 )
 SELECT
-TRIM(Product_ID),
-TRIM(Product_Name),
-		TRIM(Category),
-		TRIM(Sub_Category),
-		TRIM(Product_Tier),
-		TRIM(Is_Top_Seller),
-Standard_Cost
+	TRIM(Product_ID),
+	TRIM(Product_Name),
+	TRIM(Category),
+	TRIM(Sub_Category),
+	TRIM(Product_Tier),
+	TRIM(Is_Top_Seller),
+	Standard_Cost
 FROM NAVA_raw.dim_products
 WHERE Product_ID IS NOT NULL; -- Remove NULL Product_ID values
 
@@ -169,16 +169,16 @@ WHERE Product_ID IS NOT NULL; -- Remove NULL Product_ID values
 TRUNCATE TABLE NAVA_clean.fact_budget;
 
 INSERT INTO NAVA_clean.fact_budget (
-budget_month,
-country,
-budget_type,
-budget_amount
+	budget_month,
+	country,
+	budget_type,
+	budget_amount
 )
 SELECT
-Budget_Month,
-UPPER(TRIM(REPLACE(REPLACE(Country, CHAR(13), ''), CHAR(10), ''))),  -- Clean and Normalize Country values
-LOWER(TRIM(REPLACE(REPLACE(Budget_Type, CHAR(13), ''), CHAR(10), ''))), -- Clean and Normalize Budget_Type values
-Budget_Amount
+	Budget_Month,
+	UPPER(TRIM(REPLACE(REPLACE(Country, CHAR(13), ''), CHAR(10), ''))),  -- Clean and Normalize Country values
+	LOWER(TRIM(REPLACE(REPLACE(Budget_Type, CHAR(13), ''), CHAR(10), ''))), -- Clean and Normalize Budget_Type values
+	Budget_Amount
 FROM NAVA_raw.fact_budget
 WHERE Budget_Month IS NOT NULL; -- Remove NULL Budget_Month values
 
@@ -186,22 +186,22 @@ WHERE Budget_Month IS NOT NULL; -- Remove NULL Budget_Month values
 TRUNCATE TABLE NAVA_clean.fact_expenses;
 
 INSERT INTO NAVA_clean.fact_expenses (
-expense_id,
-invoice_date,
-department,
-cost_category,
-vendor_name,
-amount_actual,
-country
+	expense_id,
+	invoice_date,
+	department,
+	cost_category,
+	vendor_name,
+	amount_actual,
+	country
 )
 SELECT
-TRIM(Expense_ID),
-Invoice_Date,
-LOWER(TRIM(REPLACE(REPLACE(Department, CHAR(13), ''), CHAR(10), ''))), -- Clean and Normalize Department values
-LOWER(TRIM(REPLACE(REPLACE(Cost_Category, CHAR(13), ''), CHAR(10), ''))),-- Clean and Normalize Cost_Category values
-TRIM(Vendor_Name),
-Amount_Actual,
-UPPER(TRIM(REPLACE(REPLACE(Country, CHAR(13), ''), CHAR(10), ''))) -- Clean and Normalize Country values
+	TRIM(Expense_ID),
+	Invoice_Date,
+	LOWER(TRIM(REPLACE(REPLACE(Department, CHAR(13), ''), CHAR(10), ''))), -- Clean and Normalize Department values
+	LOWER(TRIM(REPLACE(REPLACE(Cost_Category, CHAR(13), ''), CHAR(10), ''))),-- Clean and Normalize Cost_Category values
+	TRIM(Vendor_Name),
+	Amount_Actual,
+	UPPER(TRIM(REPLACE(REPLACE(Country, CHAR(13), ''), CHAR(10), ''))) -- Clean and Normalize Country values
 FROM NAVA_raw.fact_expenses
 WHERE Expense_ID IS NOT NULL; -- Remove NULL Expense_ID values
 
@@ -213,30 +213,30 @@ WHERE Expense_ID IS NOT NULL; -- Remove NULL Expense_ID values
 TRUNCATE TABLE NAVA_clean.fact_marketing;
 
 INSERT INTO NAVA_clean.fact_marketing (
-marketing_id,
-`date`,
-country,
-`channel`,
-campaign_id,
-campaign_name,
-spend,
-impressions,
-clicks
+	marketing_id,
+	`date`,
+	country,
+	`channel`,
+	campaign_id,
+	campaign_name,
+	spend,
+	impressions,
+	clicks
 )
 SELECT
-TRIM(Marketing_ID),
-`Date`,
-UPPER(TRIM(REPLACE(REPLACE(Country, CHAR(13), ''), CHAR(10), ''))), -- Clean and Normalize Country values
-CASE
-WHEN TRIM(`Channel`) = 'GoogleAds' THEN 'Google Ads'
-WHEN TRIM(`Channel`) = 'MetaAds' THEN 'Meta Ads'
-ELSE TRIM(`Channel`)
-END, -- Normalize Channel values to readable format
-TRIM(Campaign_ID),
-TRIM(Campaign_Name),
-Spend,
-Impressions,
-Clicks
+	TRIM(Marketing_ID),
+	`Date`,
+	UPPER(TRIM(REPLACE(REPLACE(Country, CHAR(13), ''), CHAR(10), ''))), -- Clean and Normalize Country values
+	CASE
+		WHEN TRIM(`Channel`) = 'GoogleAds' THEN 'Google Ads'
+		WHEN TRIM(`Channel`) = 'MetaAds' THEN 'Meta Ads'
+		ELSE TRIM(`Channel`)
+	END, -- Normalize Channel values to readable format
+	TRIM(Campaign_ID),
+	TRIM(Campaign_Name),
+	Spend,
+	Impressions,
+	Clicks
 FROM NAVA_raw.fact_marketing
 WHERE Marketing_ID IS NOT NULL; -- Remove NULL Marketing_ID values
 
@@ -246,60 +246,59 @@ WHERE Marketing_ID IS NOT NULL; -- Remove NULL Marketing_ID values
 TRUNCATE TABLE NAVA_clean.fact_marketing_conversion;
 
 INSERT INTO NAVA_clean.fact_marketing_conversion (
-conversion_id,
-order_id,
-order_date,
-country,
-channel,
-campaign_id,
-campaign_name,
-customer_id,
-conversion_type,
-attributed_revenue
+	conversion_id,
+	order_id,
+	order_date,
+	country,
+	channel,
+	campaign_id,
+	campaign_name,
+	customer_id,
+	conversion_type,
+	attributed_revenue
 )
 	
 WITH deduplicated AS (
 SELECT
-conversion_id,
-order_id,
-order_date,
-country,
-channel,
-campaign_id,
-campaign_name,
-customer_id,
-conversion_type,
-attributed_revenue,
-ROW_NUMBER() OVER (
-PARTITION BY conversion_id
-ORDER BY order_date DESC) AS rn -- Keep the latest record when duplicate conversions exist
-
+	conversion_id,
+	order_id,
+	order_date,
+	country,
+	channel,
+	campaign_id,
+	campaign_name,
+	customer_id,
+	conversion_type,
+	attributed_revenue,
+	ROW_NUMBER() OVER (
+	PARTITION BY conversion_id
+	ORDER BY order_date DESC) AS rn -- Keep the latest record when duplicate conversions exist
 FROM NAVA_raw.fact_marketing_conversion
 )
 
 SELECT
-conversion_id,
-order_id,
-order_date,
-CASE
-WHEN TRIM(country) = 'FR' THEN 'FRANCE'
-WHEN TRIM(country) = 'ES' THEN 'SPAIN'
-WHEN TRIM(country) = 'PT' THEN 'PORTUGAL'
-ELSE UPPER(TRIM(country)) 
-END, -- Normalize Country values to standardized country names
-CASE
-WHEN TRIM(channel) = 'GoogleAds' THEN 'Google Ads'
-WHEN TRIM(channel) = 'Email' THEN 'Email Marketing'
-ELSE TRIM(channel)
-END, -- Normalize Channel values to readable format
-TRIM(campaign_id),
-TRIM(campaign_name),
-customer_id,
-CASE
-WHEN conversion_type IS NULL OR TRIM(conversion_type) = ''
-THEN 'Purchase'
-ELSE TRIM(conversion_type)
-END, -- Replace missing Conversion_Type values with default purchase event
-attributed_revenue 
+	conversion_id,
+	order_id,
+	order_date,
+	CASE
+		WHEN TRIM(country) = 'FR' THEN 'FRANCE'
+		WHEN TRIM(country) = 'ES' THEN 'SPAIN'
+		WHEN TRIM(country) = 'PT' THEN 'PORTUGAL'
+		ELSE UPPER(TRIM(country)) 
+	END, -- Normalize Country values to standardized country names
+	CASE
+		WHEN TRIM(channel) = 'GoogleAds' THEN 'Google Ads'
+		WHEN TRIM(channel) = 'Email' THEN 'Email Marketing'
+		ELSE TRIM(channel)
+	END, -- Normalize Channel values to readable format
+	TRIM(campaign_id),
+	TRIM(campaign_name),
+	customer_id,
+	CASE
+		WHEN conversion_type IS NULL OR TRIM(conversion_type) = ''
+		THEN 'Purchase'
+		ELSE TRIM(conversion_type)
+		END, -- Replace missing Conversion_Type values with default purchase event
+	attributed_revenue 
 FROM deduplicated
 WHERE rn = 1; -- Keep one record per Conversion_ID
